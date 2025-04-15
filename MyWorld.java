f2f28a2355dc8155f50fa4a9e7181c8b730f4352
Kalkie Eswarathasan <kalkiemithraa@gmail.com>
2025-04-15 14:11:59 -0400

    Villain's acts added.

--- MyWorld.java
+++ MyWorld.java
@@ -3,5 +3,8 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        
+        Villain villain = new Villain();
+        addObject(villain, 605, 605);
     }
 }
--- Villain.java
+++ Villain.java
@@ -1,19 +1,24 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-/**
- * Write a description of class Villain here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+
 public class Villain extends Actor
 {
-    /**
-     * Act - do whatever the Villain wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    boolean atBottom = true;
+    
     public void act()
     {
-        // Add your action code here.
+      if(Greenfoot.mouseClicked(null))
+      {
+          atBottom = !atBottom;
+      }
+      
+      if(atBottom)
+      {
+          setLocation(620, 620);
+      }
+      else 
+      {
+          setLocation(635, 635);
+      }
     }
 }
